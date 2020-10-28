1632cb8a63efae878b59410e6ad67e96046d364d
CatShark <catsharkshin@gmail.com>
2020-10-28 22:05:02 +0100

    4 részre osztás + alap css

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="inc/style.css">
+    <title>Nagy Projekt</title>
+</head>
+<body>
+    <div class="container">
+        <div class="title-box">
+            <h1>Bored in Quarantine</h1>
+        </div>
+        <div class="navbar">
+            <div class="app-title">
+                Shop
+            </div>
+            <div class="menu">
+                <a href="">Pénz</a>
+                <a href="">Név</a>
+                <a href="">Szint</a>
+            </div>
+            <div class="login-menu">
+                <a href="">Regisztráció</a>
+                <a href="">Login</a>
+                <a href="">Logout</a>
+            </div>
+        </div>
+        <div class="app-list">
+            
+        </div>
+        <div class="app-field">
+            <div class="app-container"></div>
+        </div>
+    </div>
+
+</body>
+</html>
