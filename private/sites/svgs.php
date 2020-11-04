af0e4d0462719c25e50ed14ec2b7b8aa18e4be19
CatShark <catsharkshin@gmail.com>
2020-11-04 21:55:02 +0100

    Design: Ballfish svg hozzáadása

--- private/sites/svgs.php
+++ private/sites/svgs.php
@@ -3,4 +3,5 @@
     <h1 style="color: white">Fishes</h1>
     <img style="width:50px" src="<?=PATH_SVGS.'fish.svg'?>"> <p style="color: white">Fish</p> <br>
     <img style="width:50px"  src="<?=PATH_SVGS.'fish2.svg'?>"> <p style="color: white">Fish2</p> <br>
+    <img style="width:50px"  src="<?=PATH_SVGS.'ballfish.svg'?>"> <p style="color: white">Ballfish</p> <br>
 </div>
