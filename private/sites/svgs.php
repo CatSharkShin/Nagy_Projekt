4688851fddde4168de4b7a85af784f50c1970b75
CatShark <catsharkshin@gmail.com>
2020-11-04 21:55:29 +0100

    Design: Swordfish svg hozzáadása

--- private/sites/svgs.php
+++ private/sites/svgs.php
@@ -4,4 +4,5 @@
     <img style="width:50px" src="<?=PATH_SVGS.'fish.svg'?>"> <p style="color: white">Fish</p> <br>
     <img style="width:50px"  src="<?=PATH_SVGS.'fish2.svg'?>"> <p style="color: white">Fish2</p> <br>
     <img style="width:50px"  src="<?=PATH_SVGS.'ballfish.svg'?>"> <p style="color: white">Ballfish</p> <br>
+    <img style="width:50px"  src="<?=PATH_SVGS.'swordfish.svg'?>"> <p style="color: white">Swordfish</p> <br>
 </div>
