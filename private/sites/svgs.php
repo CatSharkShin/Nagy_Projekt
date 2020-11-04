ddc8e1478ac83fa2dd38dae6be70ce3c0464c41a
CatShark <catsharkshin@gmail.com>
2020-11-04 21:55:50 +0100

    Design: Koi svg hozzáadása

--- private/sites/svgs.php
+++ private/sites/svgs.php
@@ -5,4 +5,5 @@
     <img style="width:50px"  src="<?=PATH_SVGS.'fish2.svg'?>"> <p style="color: white">Fish2</p> <br>
     <img style="width:50px"  src="<?=PATH_SVGS.'ballfish.svg'?>"> <p style="color: white">Ballfish</p> <br>
     <img style="width:50px"  src="<?=PATH_SVGS.'swordfish.svg'?>"> <p style="color: white">Swordfish</p> <br>
+    <img style="width:50px"  src="<?=PATH_SVGS.'koi.svg'?>"> <p style="color: white">Koi</p> <br>
 </div>
