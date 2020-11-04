719db84766bd176bb5310ef0abdf76c61ef62554
CatShark <catsharkshin@gmail.com>
2020-11-04 22:24:29 +0100

    Design: Kozeprehelyezes megoldasa,Svg fileok frissitese a kozepre helyezes miatt

--- private/sites/svgs.php
+++ private/sites/svgs.php
@@ -2,9 +2,13 @@
     <br>
     <h1 style="color: white">Fishes</h1>
 
-    <img style="width:50px" src="<?=PATH_SVGS.'fish.svg'?>"> <p style="color: white">Fish</p> <br>
-    <img style="width:50px"  src="<?=PATH_SVGS.'fish2.svg'?>"> <p style="color: white">Fish2</p> <br>
-    <img style="width:50px"  src="<?=PATH_SVGS.'ballfish.svg'?>"> <p style="color: white">Ballfish</p> <br>
-    <img style="width:50px"  src="<?=PATH_SVGS.'swordfish.svg'?>"> <p style="color: white">Swordfish</p> <br>
-    <img style="width:50px"  src="<?=PATH_SVGS.'koi.svg'?>"> <p style="color: white">Koi</p> <br>
+    <img style="width:50px;vertical-align: middle;" src="<?=PATH_SVGS.'fish.svg'?>"> <span style="color: white;vertical-align: middle;">Fish</span>
+    <br>
+    <img style="width:50px;vertical-align: middle;"  src="<?=PATH_SVGS.'fish2.svg'?>"> <span style="color: white;vertical-align: middle;">Fish2</span>
+    <br>
+    <img style="width:50px;vertical-align: middle;"  src="<?=PATH_SVGS.'ballfish.svg'?>"> <span style="color: white;vertical-align: middle;">Ballfish</span>
+    <br>
+    <img style="width:50px;vertical-align: middle;"  src="<?=PATH_SVGS.'swordfish.svg'?>"> <span style="color: white;vertical-align: middle;">Swordfish</span>
+    <br>
+    <img style="width:50px;vertical-align: middle;"  src="<?=PATH_SVGS.'koi.svg'?>"> <span style="color: white;vertical-align: middle;">Koi</span>
 </div>
