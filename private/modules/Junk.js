3198e7d8dd0f5446dbcad491b2894f6a30e4e695
Sirák Norbert <siraknorbert99@gmail.com>
2020-11-13 10:45:33 +0100

    Junk modul - Származtatás az Item osztályból és konstruktor leírása

---
+++ private/modules/Junk.js
@@ -0,0 +1,18 @@
+import Item from '/Item.js';
+
+// Old boot, tin can, stb.
+
+export default class Junk extends Item {
+	constructor(junk_name) {
+		// SQL lekérdezéssel megnézi, hogy van-e ilyen nevű kacat az adatbázisban
+		// Ha igen, akkor létrehoz egy példányt az adatbázisban megtalálható, adott nevű kacathoz tartozó értékekkel:
+		/*
+			if (van a kacatok táblában ilyen név) {
+				super(junk_name);
+				this.exp = db_ben_ugyanez_a_junk_exp;
+			} else {
+				exception: Nincs ilyen nevű kacat a táblában...
+			}
+		*/
+	}
+}
