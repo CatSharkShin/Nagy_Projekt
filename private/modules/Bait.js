d80cd87bac6b4c874f36ea2a83564b7a74d95b52
Sirák Norbert <siraknorbert99@gmail.com>
2020-11-13 11:32:59 +0100

    Bait modul - Származtatás az Item osztályból és konstruktor leírása

---
+++ private/modules/Bait.js
@@ -0,0 +1,16 @@
+import Item from '/Item.js';
+
+export default class Bait extends Item {
+	constructor(bait_name) {
+		// SQL lekérdezéssel megnézi, hogy van-e ilyen nevű csali az adatbázisban (Csali 1, Csali 2, ...).
+		// Ha igen, akkor létrehoz egy példányt az adatbázisban megtalálható, adott nevű csalihoz tartozó értékekkel:
+		/*
+			if (van a csalik táblában ilyen név) {
+				super(bait_name);
+				this.level = db_ben_ugyanez_a_bait_level;
+			} else {
+				exception: Nincs ilyen nevű csali a táblában...
+			}
+		*/
+	}
+}
