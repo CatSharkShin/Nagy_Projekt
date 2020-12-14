48cffa21740afb743efdc655f064a260b848d19d
Sirák Norbert <siraknorbert99@gmail.com>
2020-12-14 20:34:18 +0100

    Fishin app - ságédváltozók hozzáadása

--- public/js/fishing.js
+++ public/js/fishing.js
@@ -3,3 +3,16 @@ import Fish from '/../modules/Fish.js';
 import Junk from '/../modules/Junk.js';
 import FishingRod from '/../modules/FishingRod.js';
 import Bait from '/../modules/Bait.js';
+
+// Ezekhez drag-es eventek kellenének:
+let fishingRodEquipped = false;
+let baitEquipped = false;
+let currentFishingRod = "";
+let currentBait = "";
+
+// Szükséges html elemek eltárolása változóban:
+let exp_bar = document.getElementByClassName("exp_bar"); // betöltéskor session alapján kiírja
+let fishing_level = document.getElementByClassName("fishing_level"); //ez is session, dinamikusan változik majd 
+let fishing_button = document.getElementByClassName("fishing_button"); 
+let fish_rod_button = document.getElementByClassName("fish_rod_button");
+let bait_button = document.getElementByClassName("bait_button");
