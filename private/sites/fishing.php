7e01c372b6bc863429ba4b8562e112dea772d4e0
Sirák Norbert <siraknorbert99@gmail.com>
2020-12-14 21:59:55 +0100

    Fishing site struktúrájának átalakítása

--- private/sites/fishing.php
+++ private/sites/fishing.php
@@ -1,14 +1,24 @@
-<div class="fishing-container">
-    <div class="exp-bar">
-        <div class="exp-fill" id="exp-fill">
-        Experience bar</div>
-    </div>
-    <div class="fishing">
-        <div style="margin: auto;">
-            Fishing stuff
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="/public/css/style.css">
+        <script src="public/js/fishing.js"></script>
+    </head>
+    <body>
+        <div class="fishing-container">
+            <div class="exp-bar">
+                <div class="exp-fill" id="exp-fill">
+                Experience bar</div>
+            </div>
+            <div class="fishing">
+                <div style="margin: auto;">
+                    Fishing stuff
+                </div>
+            </div>
+            <div class="fishing-inventory">
+                Inventory
+            </div>
         </div>
-    </div>
-    <div class="fishing-inventory">
-        Inventory
-    </div>
-</div>
+    </body>
+</html>
