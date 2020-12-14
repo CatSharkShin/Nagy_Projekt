10579380e97375e3234456736c5503fd715fadb7
Sirák Norbert <siraknorbert99@gmail.com>
2020-12-14 20:32:02 +0100

    Fishing app - szükséges modulok importálása

---
+++ public/js/fishing.js
@@ -0,0 +1,5 @@
+import Item from '/../modules/Item.js';
+import Fish from '/../modules/Fish.js';
+import Junk from '/../modules/Junk.js';
+import FishingRod from '/../modules/FishingRod.js';
+import Bait from '/../modules/Bait.js';
