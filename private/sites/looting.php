96b95c65f6a0244ad625be5f12a57b63a13c81ab
CsontosDominik <61829086+CsontosDominik@users.noreply.github.com>
2020-12-14 22:08:45 +0100

    Looting app: Item táblázat alap váza

--- private/sites/looting.php
+++ private/sites/looting.php
@@ -9,6 +9,31 @@
                 itt szeretném megjeleníteni az adott itemeket 
                 amiket a karakter kap a looting során
                -->
+               <table>
+                <tbody>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                    </tr>
+                </tbody>
+                </table>
             </div>
             <div class="looting">
                 Loot
