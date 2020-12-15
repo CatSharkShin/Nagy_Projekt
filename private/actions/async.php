81e9aae60c93e44ddc1ddc763296124e5a34f8f8
CsontosDominik <61829086+CsontosDominik@users.noreply.github.com>
2020-12-15 21:20:35 +0100

    Loot rest api vázlat

--- private/actions/async.php
+++ private/actions/async.php
@@ -6,10 +6,23 @@
             case 'session':
                 getSession();
                 break;
+            case 'loot':
+                if(isset($_REQUEST['boltid']))
+                    loot();
+                break;
             default:
                 errorResponse("Action not found");
                 break;
         }
+    }
+    function loot(){
+        //minden boltnál 3 szinten lehet loot
+        //adott bolthoz adott loot 
+        //1 szint 1 item bármi
+        //2 szint 2 item bármi
+        //3 szint 3 item bármi
+        // amikor loot az adott bolthoz tartozó item lesz +1 darab
+        
     }
     function getSession(){
         $query = "SELECT money FROM users WHERE user_id = :user_id";
